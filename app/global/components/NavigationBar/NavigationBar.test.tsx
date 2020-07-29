93b0174e81aa3d1d031b0daa2e3ea2e7ed15a790
Nguyen Gia Tuan <tuan.nguyen@hubble.sg>
2020-07-29 17:21:44 +0700

    Working commit

--- app/global/components/NavigationBar/NavigationBar.test.tsx
+++ app/global/components/NavigationBar/NavigationBar.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 // RESOURCE TO TEST
+import {BrowserRouter as Router} from 'react-router-dom';
 import {RecoilRoot} from 'recoil';
 import NavigationBar from '@/global/components/NavigationBar';
 
-test('NavigationBar did show up', () => {
+test('NavigationBar did mouth', () => {
   const navigationBar = shallow(
     <RecoilRoot>
       <NavigationBar />
@@ -13,4 +14,16 @@ test('NavigationBar did show up', () => {
 
   expect(navigationBar.length).toEqual(1);
   expect(navigationBar).toMatchSnapshot();
+});
+
+test('NavigationBar show up correctly', () => {
+  const navigationBar = shallow(
+    <RecoilRoot>
+      <Router>
+        <NavigationBar />
+      </Router>
+    </RecoilRoot>,
+  );
+
+  expect(navigationBar.render().text()).toEqual('GARFDEVContact');
 });
